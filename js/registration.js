dcec943a4a64881b020528867addd93c3e162cc5
thaissa <thaiissa.muniiz@gmail.com>
2023-10-06 10:35:56 -0300

    connected to registration api

--- js/registration.js
+++ js/registration.js
@@ -2,55 +2,88 @@ import { apps } from "./index.js";
 
 const form = document.querySelector('.main__form');
 
-form.addEventListener('submit', (evt) => {
-    evt.preventDefault();
+const emailInput = document.querySelector('.email');
+const nameInput = document.querySelector('.name');
+const passwordInput = document.querySelector('.password');
+const confPassInput = document.querySelector('.confirmPass');
+const accountType = document.querySelector('#accountType');
 
-    const emailInput = document.querySelector('.email');
-    const inputName = document.querySelector('.name');
-    const passwordInput = document.querySelector('.password');
-    const confPass = document.querySelector('.confirmPass');
+const emailError = document.querySelector('.email__error');
+const nameError = document.querySelector('.name__error');
+const passError = document.querySelector('.password__error');
+const confirmPassError = document.querySelector('.confirmPassword__error');
+const accountTypeError = document.querySelector('.accountType__error');
 
-
-    if ((!apps.validateEmail(emailInput.value) || !apps.validateData(passwordInput.value)) || (passwordInput.value != confPass.value)) {
-        console.log('formato de email, senha ou nome invalidos');
-
-        if (!apps.validateEmail(emailInput.value)) {
-            console.log('insira um formato de email valido');
-        }
-        if (passwordInput.value != confPass.value) {
-            console.log('as senhas devem ser iguais');
-        }
-
-        if (inputName.value == "") {
-            console.log('Nome inválido');
-        }
-
-        if (!apps.validateEmail(passwordInput.value)) {
-            console.log('a senha deve conter no minimo 8 caracteres');
-        }
+emailInput.addEventListener('focusout', () => {
+    if (!apps.validateEmail(emailInput.value)) {
+        emailError.innerHTML = 'insira um email válido.';
+        emailError.style.display = 'block';
     } else {
-        apps.createProfile(emailInput.value, inputName.value, passwordInput.value, '', '', '').then(() => {
-            window.location.href = '../login.html'
-        });
-
-        console.log('tudo certo');
+        emailError.style.display = 'none';
     }
+});
 
-    // if (passwordInput.value != confPass.value) {
-    //     console.log('as senhas devem ser iguais');
-    // } else {
+nameInput.addEventListener('focusout', () => {
+    if (nameInput.value.length < 3) {
+        nameError.innerHTML = 'O nome deve ter pelo menos 3 letras.';
+        nameError.style.display = 'block';
+    } else {
+        nameError.style.display = 'none';
+    }
+});
 
-    //     const newDiv = document.createElement('div');
-    //     newDiv.innerHTML = 'cadastro feito com sucesso';
-    //     form.appendChild(newDiv);
-    // }
+passwordInput.addEventListener('focusout', () => {
+    if (passwordInput.value.length < 6) {
+        passError.innerHTML = 'A senha deve conter ao menos 6 caracteres.';
+        passError.style.display = 'block';
+    } else {
+        passError.style.display = 'none';
+    }
+});
+
+confPassInput.addEventListener('focusout', () => {
+    if (confPassInput.value !== passwordInput.value) {
+        confirmPassError.innerHTML = 'As duas senhas devem ser iguais.';
+        confirmPassError.style.display = 'block';
+    } else {
+        confirmPassError.style.display = 'none';
+    }
+});
+let role = "";
+
+accountType.addEventListener('focusout', () => {
+    if (accountType.value == "") {
+        accountTypeError.innerHTML = 'selecione seu tipo de conta.';
+        accountTypeError.style.display = 'block';
+    } else {
+        accountTypeError.style.display = 'none';
+    }
+    role = accountType.value == "abrigo" ? "admin" : "user";
+});
 
 
-
-    emailInput.value = '';
-    passwordInput.value = '';
-    inputName.value = '';
-    confPass.value = '';
-
-
+form.addEventListener('submit', async e => {
+    e.preventDefault();
+    if (nameInput.value.length < 3 || (!apps.validateEmail(emailInput.value) || !apps.validateData(passwordInput.value)) || (passwordInput.value != confPassInput.value)) {
+        return
+    }
+    const requestOptions = {
+        method: 'POST',
+        headers: {
+            'Content-type': 'application/json'
+        },
+        body: JSON.stringify({
+            name: nameInput.value,
+            email: emailInput.value,
+            password: passwordInput.value,
+            accountType: accountType.value,
+            role: role,
+        })
+    }
+    const response = await fetch('http://localhost:3000/users', requestOptions);
+    if (response.status == 401) {
+        alert("email já cadastrato. por favor, informe um novo email.");
+    } else {
+        alert("cadastro realizado com sucesso! vá até a pagina de login e informe suas credenciais.");
+    }
 });
